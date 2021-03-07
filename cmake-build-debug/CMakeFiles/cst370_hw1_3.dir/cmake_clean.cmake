be1ecedd05ff19e905dae483c1f905c4e0501df8
Justin Mello <mello23@llnl.gov>
2021-03-07 11:28:16 -0800

    Initial logic for loops.

---
+++ cmake-build-debug/CMakeFiles/cst370_hw1_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cst370_hw1_3.dir/hw1_3.cpp.o"
+  "cst370_hw1_3"
+  "cst370_hw1_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cst370_hw1_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hw1_3.cpp
@@ -0,0 +1,29 @@
+/*
+ * Title: hw1_3.cpp
+ * Abstract: This program reads a number of elements in a set and
+ *           then read the elements of the set. After that, the program should
+ *           display all possible binary numbers and corresponding subsets one by one.
+ * Author: Justin Mello
+ * ID: 2002
+ * Date: 03/05/2021
+ */
+
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main() {
+
+
+    int numElements = 0;
+    cin >> numElements;
+
+    string elements[numElements];
+
+    for(int i = 0; i < numElements; i++) {
+        cin >> elements[i];
+    }
+
+return 0;
+}
